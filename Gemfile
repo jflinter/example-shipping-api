1377abe0c9e4c1ad2216e63aaa254c9e8389c888
Jack Flintermann <jack@stripe.com>
2014-12-02 18:08:15 -0500

    v1 api

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 ruby "2.1.2"
 gem 'sinatra', '1.4.5'
 gem 'easypost', '2.1.0'
+gem 'json', '1.8.1'
+gem 'dotenv', '1.0.2'
--- web.rb
+++ web.rb
@@ -1,5 +1,52 @@
 require 'sinatra'
+require 'EasyPost'
+require 'dotenv'
+Dotenv.load
 
-get '/' do
-  "Hello, world"
+EasyPost.api_key = ENV['EASYPOST_API_KEY']
+
+get '/rates' do
+  content_type :json
+
+  toAddress = {
+    :street1 => params[:address_line1],
+    :street2 => params[:address_line2],
+    :city =>    params[:city],
+    :state =>   params[:state],
+    :zip =>     params[:zip],
+    :country => params[:country],
+  }
+
+  fromAddress = {
+    :company => 'EasyPost',
+    :street1 => '164 Townsend Street',
+    :street2 => 'Unit 1',
+    :city => 'San Francisco',
+    :state => 'CA',
+    :zip => '94107',
+    :phone => '415-528-7555'
+  }
+
+  parcel = {
+    :length => 9,
+    :width => 6,
+    :height => 2,
+    :weight => 10
+  }
+
+  shipment = EasyPost::Shipment.create(
+    :to_address => toAddress,
+    :from_address => fromAddress,
+    :parcel => parcel
+  )
+
+  shipment.rates.map do |rate|
+    {
+      :carrier => rate.carrier,
+      :service => rate.service,
+      :amount => rate.rate,
+      :currency => rate.currency,
+    }
+  end.to_json
+
 end
